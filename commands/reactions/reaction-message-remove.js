8a635a78228457e41d1ea0026eb21d9347ba8b33
flawstick <amirashak101@gmail.com>
2021-10-28 03:04:28 +0300

    Adding reaction rule commands

---
+++ commands/reactions/reaction-message-remove.js
@@ -0,0 +1,86 @@
+const { readFile, writeFile } = require('fs');
+const { join } = require('path');
+const { checkMessageJsonArray, checkEmojiJsonArray } = require(join(process.cwd(), '/utils/reactions.js'));
+const { Log } = require(join(process.cwd(), '/utils/log.js'));
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { reaction_roles_json, guildId } = require(join(process.cwd(), '/config.json'));
+
+module.exports = {
+
+	data: new SlashCommandBuilder()
+		.setName('reaction-role-remove')
+		.setDescription('Adds a reaction!')
+		.addStringOption(option => 
+			option.setName('message')
+				.setRequired(true)
+				.setDescription('Message ID with reaction roles.')),
+
+	async execute(interaction) {
+
+		// Get command options
+		const messageId = interaction.options.getString('message');
+
+		// Reply to message to get specefic channel
+		const reply = await interaction.reply({ content: 'Attempting to delete reaction-role', fetchReply: true });
+
+		// Fetch message using channel id from reply and given message id
+		var message = null;
+		try {
+			message = await reply.channel.messages.fetch(messageId); // Fetch message
+		} catch(error) {
+
+			// Log error and delete message.
+			Log(`[ERROR] [REACTION ROLES COMMAND] ${error} `);
+			await reply.edit({ content: 'Could not find message' });
+			reply.delete({timeout: 100000});
+			return;
+		}
+		
+		try {
+			await message.reactions.removeAll(); // Remove reactions from the messsage
+		} catch (error) {
+
+			// Log error and delete message.
+			Log(`[ERROR] [REACTION ROLES COMMAND] ${error} `);
+			await reply.edit({ content: 'Could not find emoji' });
+			reply.delete({timeout: 100000});
+			return;
+		}
+
+		// Add to json
+		readFile(reaction_roles_json, 'utf-8', (err, data) => {
+			if (err) throw err; // catch error
+
+			// Load json data
+			const reaction_roles = JSON.parse(data);
+
+			// Check the existence of the gven message
+			const existance = checkMessageJsonArray(reaction_roles["messages"] , messageId);
+			if (existance === false) {
+
+                // Log error and delete message.
+			    Log(`[ERROR] [REACTION ROLES COMMAND] [REMOVE] [MESSAGE] Message ${messageId} does not contain any reaction roles!`);
+			    reply.edit({ content: 'Message is not a reaction role message!' });
+			    reply.delete({timeout: 100000});
+			    return;
+            }
+
+			// Remove emoji from json message	
+			reaction_roles.messages = reaction_roles.messages.splice(existance, 1);
+
+			// Log to console
+			Log("[INFO] [REACTION ROLES COMMAND] [REMOVE] [MESSAGE] Removed message: " + messageId);
+			
+			// Write the Json back into the file
+			writeFile(reaction_roles_json, JSON.stringify(reaction_roles), (err) => {
+                if (err) throw err; // Catch error
+			});
+			
+		});
+
+		// Inform of success
+		reply.edit(`Reaction-Message! ${messageId} removed!`);
+		reply.delete({timeout: 100000});
+	}
+};
---
+++ commands/reactions/reaction-role-remove.js
@@ -0,0 +1,102 @@
+const { readFile, writeFile } = require('fs');
+const { join } = require('path');
+const { checkMessageJsonArray, checkEmojiJsonArray } = require(join(process.cwd(), '/utils/reactions.js'));
+const { Log } = require(join(process.cwd(), '/utils/log.js'));
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { reaction_roles_json } = require(join(process.cwd(), '/config.json'));
+
+module.exports = {
+
+	data: new SlashCommandBuilder()
+		.setName('reaction-role-remove')
+		.setDescription('Adds a reaction!')
+		.addStringOption(option => 
+			option.setName('message')
+				.setRequired(true)
+				.setDescription('Message ID with reaction roles.'))
+		.addStringOption(option => 
+			option.setName('emoji')
+				.setRequired(true)
+				.setDescription('Emoji which adds the role')),
+
+	async execute(interaction) {
+
+		// Get command options
+		const messageId = interaction.options.getString('message');
+		const emoji = interaction.options.getString('emoji');
+
+		// Reply to message to get specefic channel
+		const reply = await interaction.reply({ content: 'Attempting to delete reaction-role', fetchReply: true });
+
+		// Fetch message using channel id from reply and given message id
+		var message = null;
+		try {
+			message = await reply.channel.messages.fetch(messageId); // Fetch message
+		} catch(error) {
+
+			// Log error and delete message.
+			Log(`[ERROR] [REACTION ROLES COMMAND] ${error} `);
+			await reply.edit({ content: 'Could not find message' });
+			reply.delete({timeout: 100000});
+			return;
+		}
+		
+		try {
+			await message.reactions.cache.get(emoji.id).remove(); // Remove reactions from the messsage
+		} catch (error) {
+
+			// Log error and delete message.
+			Log(`[ERROR] [REACTION ROLES COMMAND] ${error} `);
+			await reply.edit({ content: 'Could not find emoji' });
+			reply.delete({timeout: 100000});
+			return;
+		}
+
+		// Add to json
+		readFile(reaction_roles_json, 'utf-8', (err, data) => {
+			if (err) throw err; // catch error
+
+			// Load json data
+			const reaction_roles = JSON.parse(data);
+
+			// Check the existence of the gven message
+			const existance = checkMessageJsonArray(reaction_roles["messages"] , messageId);
+			if (existance === false) {
+
+                // Log error and delete message.
+			    Log(`[ERROR] [REACTION ROLES COMMAND] [REMOVE] [ROLE] Message ${messageId} does not contain any reaction roles!`);
+			    reply.edit({ content: 'Message is not a reaction role message!' });
+			    reply.delete({timeout: 100000});
+			    return;
+            }
+
+            // Check existance of the role 
+            const emojiExistance = checkEmojiJsonArray(reaction_roles["messages"], emoji.toString(), existance, messageId);
+            if (emojiExistance === false) {
+
+                // Log error and delete message.
+			    Log(`[ERROR] [REACTION ROLES COMMAND] [REMOVE] [ROLE] Message does not contain ${emoji.toString()}!`);
+			    reply.edit({ content: 'Message does not contain ' + emoji.toString() });
+			    reply.delete({timeout: 100000});
+			    return;   
+            }
+
+			// Remove emoji from json message	
+			reaction_roles.messages = reaction_roles.messages[existance][messageId].splice(emojiExistance, 1);
+
+			// Log to console
+			Log("[INFO] [REACTION ROLES COMMAND] [REMOVE] [REACTION] Removed " + emoji.toString() + " from message: " + messageId);
+			
+			// Write the Json back into the file
+			writeFile(reaction_roles_json, JSON.stringify(reaction_roles), (err) => {
+                if (err) throw err; // Catch error
+			});
+			
+		});
+
+		// Inform of success
+		reply.edit(`Reaction-Role! ${emoji} removed!`);
+		reply.delete({timeout: 100000});
+	}
+};
